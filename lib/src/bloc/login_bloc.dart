6330e72462a397fce7a79d3dbfd4ded75733dd88
Irving Didier <bider88@gmail.com>
2020-12-02 23:24:40 -0600

    add singleton pattern to Provider

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -21,6 +21,10 @@ class LoginBloc with Validators {
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
+  // get last value
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
+
   dispose() {
     _emailController?.close();
     _passwordController?.close();
--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -4,10 +4,21 @@ export 'package:formularios_flutter/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget{
 
+
+  static Provider _instance;
+
+  factory Provider({ Key key, Widget child }) {
+    if (_instance == null) {
+      _instance = new Provider._internal(key: key, child: child);
+    }
+
+    return _instance;
+  }
+
+  Provider._internal({ Key key, Widget child }): super(key: key, child: child);
+
   final loginBloc = LoginBloc();
-
-  Provider({ Key key, Widget child }): super(key: key, child: child);
-
+  
   @override // Debe de actualizar a los hijos?
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
 
